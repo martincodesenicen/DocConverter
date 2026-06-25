edb9d24c3dbf8edae307592f0cb5a033eef029bd
martincodesenicen <martincodesenicen@gmail.com>
2026-06-24 21:31:05 -0300

    feat(layout): add breadcrumbs and navigation actions

--- doc-converter-web/src/app/shared/components/conversion-layout/conversion-layout.component.ts
+++ doc-converter-web/src/app/shared/components/conversion-layout/conversion-layout.component.ts
@@ -9,12 +9,25 @@ import {
   MatCardModule
 } from '@angular/material/card';
 
+import { RouterLink } from '@angular/router';
+
+import {
+  MatButtonModule
+} from '@angular/material/button';
+
+import {
+  MatIconModule
+} from '@angular/material/icon';
+
 @Component({
   selector: 'app-conversion-layout',
   standalone: true,
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './conversion-layout.component.html',
   styleUrl: './conversion-layout.component.scss'
